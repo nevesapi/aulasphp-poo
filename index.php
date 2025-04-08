521633b4f19b0d4fda0f8d31a35b6c35a1a31946
Denis Neves <nevesdenis35@gmail.com>
2025-04-08 11:22:41 -0300

    Acessa recursos

--- index.php
+++ index.php
@@ -10,7 +10,19 @@
 <body>
   <h1>PHP com POO - Exemplo 01</h1>
   <hr>
-  
+
+  <?php
+  require_once "src/Cliente.php";
+
+  //Criando instancia da classe
+  $clienteA = new Cliente();
+  $clienteB = new Cliente();
+
+  ?>
+
+  <h2>Testes de acessos aos recursos da classe atravé sdo objeto</h2>
+  <pre><?= var_dump($clienteA) ?></pre>
+  <pre><?= var_dump($clienteB) ?></pre>
 </body>
 
 </html>
