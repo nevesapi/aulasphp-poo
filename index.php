54753b578716de87c4bebbd8e2668bf463d9ce35
Denis Neves <nevesdenis35@gmail.com>
2025-04-17 10:03:22 -0300

    Cria objeto pessoa juridica

--- index.php
+++ index.php
@@ -15,10 +15,14 @@
   require_once "./src/Situacao.php"; //enum class
   require_once "./src/Cliente.php"; //main class
   require_once "./src/PessoaFisica.php"; //sub class
+  require_once "./src/PessoaJuridica.php"; //sub class
+
   $clientePF = new PessoaFisica("Teste", "teste@exemplo.com", 20, "123.456.789-00");
+  $clientePJ = new PessoaJuridica("Denis", "tenis@exemplo.com", "12.345.6789/0001", 2025, "Bilola")
   ?>
 
   <pre><?= var_dump($clientePF) ?></pre>
+  <pre><?= var_dump($clientePJ) ?></pre>
 </body>
 
 </html>
---
+++ src/PessoaJuridica.php
@@ -0,0 +1,42 @@
+<?php
+class PessoaJuridica extends Cliente
+{
+  private string $cnpj;
+  private int $anoFundacao;
+  private string $nomeFantasia;
+
+  public function __construct(string $nome, string $email, string $cnpj, int $anoFundacao, string $nomeFantasia)
+  {
+    parent::__construct($nome, $email);
+
+    $this->setCnpj($cnpj);
+    $this->setAnoFundacao($anoFundacao);
+    $this->setNomeFantasia($nomeFantasia);
+  }
+
+  private function setCnpj(string $cnpj): void
+  {
+    $this->cnpj = $cnpj;
+  }
+  private function setAnoFundacao(int $anoFundacao): void
+  {
+    $this->anoFundacao = $anoFundacao;
+  }
+  private function setNomeFantasia(string $nomeFantasia): void
+  {
+    $this->nomeFantasia = $nomeFantasia;
+  }
+
+  public function getCnpj(): string
+  {
+    return $this->cnpj;
+  }
+  public function getAnoFundacao(): int
+  {
+    return $this->anoFundacao;
+  }
+  public function getNomeFantasia(): string
+  {
+    return $this->nomeFantasia;
+  }
+}
